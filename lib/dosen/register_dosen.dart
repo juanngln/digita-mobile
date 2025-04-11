3d6a4d90cbaaf5c76c3df926b60f72b61a4eed04
juanngln <juanjonathan05@gmail.com>
2025-04-11 12:47:47 +0700

    Create Feat: Login & Register

---
+++ lib/dosen/register_dosen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterDosenPage extends StatelessWidget {
+  const RegisterDosenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register Dosen'),
+      ),
+      body: const Center(
+        child: Text('Register Dosen Page'),
+      ),
+    );
+  }
+}
---
+++ lib/mahasiswa/register_mahasiswa.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterMahasiswa extends StatelessWidget {
+  const RegisterMahasiswa({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register Dosen'),
+      ),
+      body: const Center(
+        child: Text('Register Dosen Page'),
+      ),
+    );
+  }
+}
